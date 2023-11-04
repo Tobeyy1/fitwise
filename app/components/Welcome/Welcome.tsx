7b8d351182c62ba8c5fef1952a619f832f47ccdf
Tobeyy1 <tobeyawaagwu.taa@gmail.com>
2023-11-04 02:18:59 +0100

    Added Welcome Video

--- app/components/Welcome/Welcome.tsx
+++ app/components/Welcome/Welcome.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import React from "react";
 import classes from "./Welcome.module.scss";
 import { BsArrowRightShort } from "react-icons/bs";
 import Link from "next/link";
+// import BG_VIDEO from "../../assets/videos/workout_welcome_screen.mp4"
 
 const Welcome = () => {
+  const videoURL = "/videos/workout_welcome_screen.mp4";
   return (
     <div className={classes.container}>
+      <div className={classes.video__container}>
+        <video
+          src={videoURL}
+          autoPlay
+          loop
+          // width={window.innerWidth}
+          // height={window.innerHeight}
+          typeof="video/mp4"
+        />
+      </div>
       <h1>FitWise</h1>
       <Link href="/onboarding">
         Get Started{" "}
