d09252c998d15bce754b85dccb1f72aaf47faebc
Tobeyy1 <tobeyawaagwu.taa@gmail.com>
2023-11-11 15:31:05 +0100

    Fixed error in Slug

--- app/system/shop/[slug]/page.tsx
+++ app/system/shop/[slug]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./ItemDisplay.module.scss";
 
 const ItemDisplay = () => {
-  return <div>I've not done this page yet</div>;
+  return <div>I&apos;ve not done this page yet</div>;
 };
 
 export default ItemDisplay;
