49f19e31e2a5d32d90ba3d6e60cb055dc5784c39
Tobeyy1 <tobeyawaagwu.taa@gmail.com>
2023-11-12 22:07:45 +0100

    Added Full Screen Feature on Slug Page

---
+++ app/system/shop/[slug]/FullScreenImage.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./ItemDisplay.module.scss";
+import Image, { StaticImageData } from "next/image";
+import { BsFullscreenExit } from "react-icons/bs";
+import { motion } from "framer-motion";
+interface Props {
+  imageSrc: StaticImageData;
+  onExit: () => void;
+}
+
+const FullScreenImage: React.FC<Props> = ({ imageSrc, onExit }) => {
+  return (
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      exit={{ scale: 0.5, opacity: 0 }}
+      transition={{ type: "tween" }}
+      className={classes.fullscreen__container}
+    >
+      <div className={classes.exit__full__screen} onClick={onExit}>
+        <span className={classes.icon}>
+          {" "}
+          <BsFullscreenExit />
+        </span>{" "}
+        <p>Exit Full Screen</p>
+      </div>
+      <Image src={imageSrc} alt="Item" fill className={classes.image} />
+    </motion.div>
+  );
+};
+
+export default FullScreenImage;
--- app/system/shop/[slug]/page.tsx
+++ app/system/shop/[slug]/page.tsx
@@ -1,36 +1,86 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import classes from "./ItemDisplay.module.scss";
 import Image from "next/image";
 import TRAINER from "../../../assets/images/nike-trainer.jpg";
 import { TbArrowLeft } from "react-icons/tb";
 import { HiShoppingCart } from "react-icons/hi";
 import { BsFullscreen } from "react-icons/bs";
+import { useRouter } from "next/navigation";
+import FullScreenImage from "./FullScreenImage";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ItemDisplay = () => {
+  const [isFullScreenActive, setIsFullScreenActive] = useState<boolean>(false);
+  const router = useRouter();
+  const exitFullScreenHandler = () => {
+    setIsFullScreenActive(false);
+  };
   return (
     <div className={classes.container}>
-      {" "}
+      <AnimatePresence>
+        {isFullScreenActive && (
+          <FullScreenImage imageSrc={TRAINER} onExit={exitFullScreenHandler} />
+        )}
+      </AnimatePresence>
       <div className={classes.top__bar}>
-        <span className={classes.icon}>
+        <span className={classes.icon} onClick={() => router.back()}>
           {" "}
           <TbArrowLeft />{" "}
         </span>
-        <h3>Shoes</h3>
+        <h3>Nike Trainers</h3>
         <span className={classes.icon}>
           {" "}
           <HiShoppingCart />
         </span>
       </div>
-      <div className={classes.image__container}>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.5, opacity: 0 }}
+        transition={{ type: "tween" }}
+        className={classes.image__container}
+      >
         <Image src={TRAINER} className={classes.image} alt="Item " fill />{" "}
-        <div className={classes.full__screen}>
+        <div
+          className={classes.full__screen}
+          onClick={() => setIsFullScreenActive(true)}
+        >
           <span className={classes.icon}>
             {" "}
             <BsFullscreen />
           </span>{" "}
           <p>Full Screen</p>
         </div>
-      </div>
+      </motion.div>{" "}
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 100, opacity: 0 }}
+        transition={{ delay: 0.25, type: "tween" }}
+        className={classes.item__details}
+      >
+        <p>Color</p> <span className={classes.value}>Blue</span>
+      </motion.div>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 100, opacity: 0 }}
+        transition={{ delay: 0.35, type: "tween" }}
+        className={classes.item__details}
+      >
+        <p>Size</p> <span className={classes.value}>45</span>
+      </motion.div>
+      <motion.button
+        initial={{ y: "100%", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: "100%", opacity: 0 }}
+        transition={{ delay: 0.45, type: "tween" }}
+        type="button"
+        className={classes.add__to__cart}
+      >
+        ADD TO CART - <span>N15,000</span>
+      </motion.button>
     </div>
   );
 };
